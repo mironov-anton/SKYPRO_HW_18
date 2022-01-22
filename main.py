48c6291f13f1e96a6d89a6a7e2cf3a8d8d5fa5f1
mironov-anton <miron.khuzd@gmail.com>
2022-01-22 14:26:30 +0300

    main.py ready

--- main.py
+++ main.py
@@ -4,27 +4,26 @@ from flask_restx import Api
 from app.config import Config
 from app.setup_db import db
 from app.views.directors import director_ns
-from app.views import genre_ns
+from app.views.genres import genre_ns
 from app.views.movies import movie_ns
 
 
-def create_app(config_object):
-    app = Flask(__name__)
-    app.config.from_object(config_object)
-    register_extensions(app)
-    return app
+def create_app(config_object: Config):
+    application = Flask(__name__)
+    application.config.from_object(config_object)
+    application.app_context().push()
+    configure_app(application)
+    return application
 
 
-def register_extensions(app):
-    db.init_app(app)
-    api = Api(app)
+def configure_app(application: Flask):
+    db.init_app(application)
+    api = Api(application)
     api.add_namespace(director_ns)
     api.add_namespace(genre_ns)
     api.add_namespace(movie_ns)
 
 
-app = create_app(Config())
-app.debug = True
-
 if __name__ == '__main__':
-    app.run(host="localhost", port=10001, debug=True)
+    app = create_app(Config())
+    app.run()  # debug set in config
