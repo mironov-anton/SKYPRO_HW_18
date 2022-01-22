8184ce4a7f94d97f0bdcac2f5eade017aacaa81c
mironov-anton <miron.khuzd@gmail.com>
2022-01-22 17:20:01 +0300

    basic CRUD completed for views

--- app/views/genres.py
+++ app/views/genres.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restx import Resource, Namespace
 
 from app.dao.models.genre import GenreSchema
@@ -5,18 +6,42 @@ from app.container import genre_service
 
 genre_ns = Namespace('genres')
 
+genre_schema = GenreSchema()
+genres_schema = GenreSchema(many=True)
+
 
 @genre_ns.route('/')
 class GenresView(Resource):
     def get(self):
-        rs = genre_service.get_all()
-        res = GenreSchema(many=True).dump(rs)
-        return res, 200
+        all_genres = genre_service.get_all()
+        return genres_schema.dump(all_genres), 200
+
+    def post(self):
+        req_json = request.json
+        genre = genre_service.create(req_json)
+        return "", 201, {"location": f"/genres/{genre.id}"}
 
 
-@genre_ns.route('/<int:rid>')
+@genre_ns.route('/<int:gid>')
 class GenreView(Resource):
-    def get(self, rid):
-        r = genre_service.get_one(rid)
-        sm_d = GenreSchema().dump(r)
-        return sm_d, 200
+    def get(self, gid: int):
+        genre = genre_service.get_one(gid)
+        return genre_schema.dump(genre), 200
+
+    def put(self, gid: int):
+        req_json = request.json
+        if "id" not in req_json:
+            req_json["id"] = gid
+        genre_service.update(req_json)
+        return "", 204
+
+    def patch(self, gid: int):
+        req_json = request.json
+        if "id" not in req_json:
+            req_json["id"] = gid
+        genre_service.update_partial(req_json)
+        return "", 204
+
+    def delete(self, gid: int):
+        genre_service.delete(gid)
+        return "", 204
--- app/views/movies.py
+++ app/views/movies.py
@@ -6,21 +6,24 @@ from app.container import movie_service
 
 movie_ns = Namespace('movies')
 
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
 
 @movie_ns.route('/')
 class MoviesView(Resource):
     def get(self):
-        director = request.args.get("director_id")
-        genre = request.args.get("genre_id")
-        year = request.args.get("year")
-        filters = {
-            "director_id": director,
-            "genre_id": genre,
-            "year": year,
-        }
-        all_movies = movie_service.get_all(filters)
-        res = MovieSchema(many=True).dump(all_movies)
-        return res, 200
+        # director = request.args.get("director_id")
+        # genre = request.args.get("genre_id")
+        # year = request.args.get("year")
+        # filters = {
+        #     "director_id": director,
+        #     "genre_id": genre,
+        #     "year": year,
+        # }
+        # all_movies = movie_service.get_all(filters)
+        all_movies = movie_service.get_all()
+        return movies_schema.dump(all_movies), 200
 
     def post(self):
         req_json = request.json
@@ -28,20 +31,26 @@ class MoviesView(Resource):
         return "", 201, {"location": f"/movies/{movie.id}"}
 
 
-@movie_ns.route('/<int:bid>')
+@movie_ns.route('/<int:mid>')
 class MovieView(Resource):
-    def get(self, bid):
-        b = movie_service.get_one(bid)
-        sm_d = MovieSchema().dump(b)
-        return sm_d, 200
+    def get(self, mid):
+        movie = movie_service.get_one(mid)
+        return movie_schema.dump(movie), 200
 
-    def put(self, bid):
+    def put(self, mid):
         req_json = request.json
         if "id" not in req_json:
-            req_json["id"] = bid
+            req_json["id"] = mid
         movie_service.update(req_json)
         return "", 204
 
-    def delete(self, bid):
-        movie_service.delete(bid)
+    def patch(self, mid: int):
+        req_json = request.json
+        if "id" not in req_json:
+            req_json["id"] = mid
+        movie_service.update_partial(req_json)
+        return "", 204
+
+    def delete(self, mid):
+        movie_service.delete(mid)
         return "", 204
